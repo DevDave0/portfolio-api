9845d61634ec4375c3b3bea6780111f984cc016c
devdave0 <dbchung24@gmail.com>
2020-11-19 19:31:35 -0500

    set up variables for node;

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('bodyParser');
+const cors = require('cors');
+
+const sendGrid = require('@sendgrid/mail');
+
+
+const app = express();
