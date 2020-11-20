18ccdbaa2436c8b4bf7eeff7341744c929639204
devdave0 <dbchung24@gmail.com>
2020-11-19 19:44:10 -0500

    set node server

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const sendGrid = require('@sendgrid/mail');
 
 
 const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.get('/api', (req, res, next) => {
+    res.send('API Status: Running')
+});
+
+app.listen(3030, '0.0.0.0');
